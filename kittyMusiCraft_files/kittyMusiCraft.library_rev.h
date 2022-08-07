e4a1e3673a38cf9efdef3b8291aa9a2aaaf81a3e
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2022-08-07 19:59:18 +0200

    Fixed version number and date

--- kittyMusiCraft_files/kittyMusiCraft.library_rev.h
+++ kittyMusiCraft_files/kittyMusiCraft.library_rev.h
@@ -1,6 +1,10 @@
-#define VERSION		53
-#define REVISION	1
-#define DATE		"27.6.2022"
-#define VERS		"kittyMusiCraft.library 53.1"
-#define VSTRING		"kittyMusiCraft.library 53.1 (27.6.2022)\r\n"
-#define VERSTAG		"\0$VER: kittyMusiCraft.library 53.1 (27.6.2022)"
+
+#define VERSION 1
+#define REVISION 1
+
+#define VER "1.1"
+#define DATE		"7.8.2022"
+#define VERS		"kittyMusiCraft.library " VER
+#define VSTRING		"kittyMusiCraft.library " VER " (" DATE ")\r\n"
+#define VERSTAG		"\0$VER: kittyMusiCraft.library " VER " (" DATE ")"
+
