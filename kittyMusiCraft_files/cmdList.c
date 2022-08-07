44e288ee93973b02abc4089b681cabfd85d3abb3
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2022-08-07 17:52:02 +0200

    Corrected offsets.

--- kittyMusiCraft_files/cmdList.c
+++ kittyMusiCraft_files/cmdList.c
@@ -14,17 +14,18 @@
 #include "cmdList.h"
 
 struct cmdData AMOSPro_MusiCRAFT[]={
-		{0x0002,"St Load",0,musicraftStLoad},
-		{0x0012,"St Play",0,musicraftStPlay},
-		{0x0024,"St Play",0,musicraftStPlay},
-		{0x002C,"St Stop",0,musicraftStStop},
-		{0x003A,"St Pause On",0,musicraftStPauseOn},
-		{0x004C,"St Pause Off",0,musicraftStPauseOff},
-		{0x005E,"St Voice",0,musicraftStVoice},
-		{0x006E,"St Channel",0,musicraftStChannel},
-		{0x0080,"St Vumeter Speed",0,musicraftStVumeterSpeed},
-		{0x0098,"St Volume",0,musicraftStVolume},
-		{0x00A8,"St Get Volume",0,musicraftStGetVolume},
-		{0x00BC,"St Base",0,musicraftStBase},
-		{0x00CA,"St Version",0,musicraftStVersion},
+		{0x0006,"St Load",0,musicraftStLoad},
+		{0x0016,"St Play",0,musicraftStPlay},
+		{0x0028,"St Play",0,musicraftStPlay},
+		{0x0030,"St Stop",0,musicraftStStop},
+		{0x003E,"St Pause On",0,musicraftStPauseOn},
+		{0x0050,"St Pause Off",0,musicraftStPauseOff},
+		{0x0062,"St Voice",0,musicraftStVoice},
+		{0x0072,"St Channel",0,musicraftStChannel},
+		{0x0084,"St Vumeter Speed",0,musicraftStVumeterSpeed},
+		{0x009C,"St Volume",0,musicraftStVolume},
+		{0x00AC,"St Get Volume",0,musicraftStGetVolume},
+		{0x00C0,"St Base",0,musicraftStBase},
+		{0x00CE,"St Version",0,musicraftStVersion},
 		{0x0000,NULL,0,0}};
+
